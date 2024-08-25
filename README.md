7cb4917d851f5a45ffca0a0742111c5e5e0b9865
Stelios Pnevmatikakis <63586679+Steliospne@users.noreply.github.com>
2024-08-25 18:55:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project-Homepage
+
+Live url:
---
+++ dist/index.bundle.js
@@ -0,0 +1,16 @@
+(() => {
+  var r = {
+      954: (r) => {
+        r.exports = function (r) {
+          return r[0].toUpperCase() + r.slice(1);
+        };
+      },
+    },
+    e = {};
+  !(function t(o) {
+    var n = e[o];
+    if (void 0 !== n) return n.exports;
+    var p = (e[o] = { exports: {} });
+    return r[o](p, p.exports, t), p.exports;
+  })(954);
+})();
