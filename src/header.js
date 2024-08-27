bc70d564909ca1d4fbe1cbc40d74090022e733af
Steliospne <63586679+Steliospne@users.noreply.github.com>
2024-08-27 08:05:48 +0200

    First layout implementation of the header element

--- src/header.js
+++ src/header.js
@@ -1,10 +1,36 @@
 module.exports = function headerComponent() {
   const header = document.createElement("header");
-  const heroSrc = require("./assets/images/hero.jpg");
+  const wrapper = document.createElement("div");
+  const imageWrapper = document.createElement("div");
+  const imgSrc = require("./assets/images/hero.jpg");
+  const image = document.createElement("img");
 
-  header.innerHTML = `
-  <img class="heroImg" src="${heroSrc}">
-  `;
+  const name = document.createElement("h1");
+  const descriptionContainer = document.createElement("div");
+  const descriptionTitle = document.createElement("h2");
+  const descriptionContent = document.createElement("p");
+
+  header.className = "header";
+  wrapper.className = "wrapper";
+  imageWrapper.className = "img-wrapper";
+  image.src = imgSrc;
+  image.className = "image";
+
+  name.className = "name";
+  descriptionContainer.className = "desc-container";
+
+  name.textContent = "Your Name";
+  descriptionTitle.textContent = "About me";
+  descriptionContent.textContent = `
+  Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+  Quas velit officiis reprehenderit et optio consequatur, 
+  itaque quaerat veritatis assumenda vitae eaque iure tempora 
+  temporibus laborum. Libero quam fuga pariatur consequuntur?`;
+
+  wrapper.append(imageWrapper, descriptionContainer);
+  imageWrapper.append(image, name);
+  descriptionContainer.append(descriptionTitle, descriptionContent);
+  header.append(wrapper);
 
   return header;
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import headerComponent from "./header";
+import sectionComponent from "./section";
+import footerComponent from "./footer";
 import "./style.css";
 
 window.onload = () => {
-  document.body.append(headerComponent());
+  const app = document.createElement("div");
+  const main = document.createElement("main");
+
+  app.className = "app";
+
+  main.append(headerComponent());
+  main.append(sectionComponent());
+
+  app.append(main, footerComponent());
+  document.body.append(app);
 };
