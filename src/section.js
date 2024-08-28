357ae523dfd614ae56d013538e7ed7ca118bda24
Steliospne <63586679+Steliospne@users.noreply.github.com>
2024-08-28 20:38:08 +0200

    Modifies the section module to generate cards by passing the card data from an object

--- src/section.js
+++ src/section.js
@@ -4,26 +4,42 @@ module.exports = function section() {
   const section = document.createElement("section");
   const title = document.createElement("h1");
   const showcase = document.createElement("div");
-  title.textContent = "Showcasing";
 
+  title.textContent = "Showcasing";
   showcase.className = "showcase";
 
-  for (let i = 0; i < 4; i++) {}
-  const gitIcon =
-    "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg";
-  const gitUrl = "https://github.com/Steliospne/Project-Battleship";
-  const openIcon = "./assets/images/open-in-new.png";
-  const openUrl = "https://steliospne.github.io/Project-Battleship/";
-  const card = new Card();
-  card.setTitle("Project 1");
-  card.setImage("./assets/images/battleship.jpg");
-  card.setButton(openIcon, openUrl);
-  card.setButton(gitIcon, gitUrl);
-  const cardEl = card.getCard();
-  showcase.append(cardEl);
+  const data = {
+    0: {
+      name: "Battleship",
+      image: "./assets/images/battleship.jpg",
+      git_icon:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg",
+      git_url: "https://github.com/Steliospne/Project-Battleship",
+      open_icon: "./assets/images/open-in-new.png",
+      open_url: "https://steliospne.github.io/Project-Battleship/",
+    },
+    // 1: {
+    //   name: "",
+    //   image: "./assets/images/battleship.jpg",
+    //   git_icon:
+    //     "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg",
+    //   git_url: "https://github.com/Steliospne/Project-Battleship",
+    //   open_icon: "./assets/images/open-in-new.png",
+    //   open_url: "https://steliospne.github.io/Project-Battleship/",
+    // },
+  };
 
-  // showcase.addEventListener("click", cardHandler);
-  // section.append(title, showcase);
+  const projects = Object.values(data);
+  for (let i = 0; i < 1; i++) {
+    const card = new Card();
+    card.setTitle(projects[i].name);
+    card.setImage(projects[i].image);
+    card.setButton(projects[i].open_icon, projects[i].open_url);
+    card.setButton(projects[i].git_icon, projects[i].git_url);
+    const cardEl = card.getCard();
+    showcase.append(cardEl);
+  }
+
   section.append(showcase);
 
   return section;
